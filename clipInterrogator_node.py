963b8acc9c7f7691a7067475dccf595c5abccc68
Trase1 <trase93@gmail.com>
2023-12-12 16:53:12 +0300

    Added check for installed clip-interrogator Added support for SDXL Added printing of a prompt to a console Now lowVram is global

--- clipInterrogator_node.py
+++ clipInterrogator_node.py
@@ -3,8 +3,15 @@ from invokeai.app.invocations.primitives import ImageField, StringOutput, String
 from invokeai.app.invocations.model import UNetField
 from invokeai.backend.model_management import BaseModelType
 from PIL import Image
-from clip_interrogator import Config, Interrogator
 import torch
+
+try:
+    from clip_interrogator import Config, Interrogator
+except ImportError:
+    print("clip-interrogator is not installed, please install it with 'pip install clip-interrogator'")
+    exit(1)
+
+
 
 @invocation("CLIPInterrogator", title="CLIP Interrogator", tags=["CLIP", "prompt", "interrogation"], version="0.1.0")
 class clipInterrogatorInvocation(BaseInvocation):
@@ -15,7 +22,6 @@ class clipInterrogatorInvocation(BaseInvocation):
    
     def invoke(self, context: InvocationContext) -> StringOutput :
         clipModelName: str = ''
-        lowVram = False
         device: str = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         if not torch.cuda.is_available():
             print("CUDA is not available, using CPU. Warning: this will be very slow!")
@@ -23,12 +29,14 @@ class clipInterrogatorInvocation(BaseInvocation):
             clipModelName = 'ViT-L-14/openai'
         elif self.unet.unet.base_model == BaseModelType.StableDiffusion2:
             clipModelName = 'ViT-H-14/laion2b_s32b_b79k'
+        elif self.unet.unet.base_model == BaseModelType.StableDiffusionXL:
+            clipModelName = 'ViT-bigG-14/laion2b_s39b_b160k'
         else : 
-            raise Exception("Error: not compatible StableDiffusion version")
-        config = Config(device=device, clip_model_name=clipModelName)
-        if lowVram == True : config.apply_low_vram_defaults()
+            raise Exception("Error: not compatible StableDiffusion(?) version")
+        config = Config(device=device, clip_model_name=clipModelName)    
+        if self.lowVram == True : config.apply_low_vram_defaults()
         image = context.services.images.get_pil_image(self.image.image_name).convert('RGB')
         ci = Interrogator(config)
         prompt = ci.interrogate(image)
-        
+        print(prompt)
         return StringOutput(value = prompt)
